2f67fc363030f0469585028d2567e03a4f8b1455
gilSFranco <gilmarsoaresfranco@gmail.com>
2025-02-16 18:59:50 -0300

    feat: standardizing the colors and typography of the application

--- app/src/main/java/com/banap/banap/ui/theme/Color.kt
+++ app/src/main/java/com/banap/banap/ui/theme/Color.kt
@@ -2,4 +2,10 @@ package com.banap.banap.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val VERDE_CLARO = Color(0xFF1EA81E)
+val VERDE_ESCURO = Color(0xFF1A5D1A)
+val PRETO = Color(0xFF000000)
 val BRANCO = Color(0xFFFFFFFF)
+val CINZA_CLARO = Color(0xFFE0E0E0)
+val CINZA_ESCURO = Color(0xFF828282)
+val VERMELHO = Color(0xFFC4302B)
--- app/src/main/java/com/banap/banap/ui/theme/Type.kt
+++ app/src/main/java/com/banap/banap/ui/theme/Type.kt
@@ -1,6 +1,25 @@
 package com.banap.banap.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 val Typography = Typography(
+    // Usar no textBox e no "Esqueceu sua senha?" na tela de Login
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontSize = 14.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Light,
+        fontSize = 20.sp
+    )
 )
