487f42353e8675330a968dd3c6593f600e86e42a
gilSFranco <gilmarsoaresfranco@gmail.com>
2025-02-16 18:51:23 -0300

    feat: banap project created

---
+++ app/src/main/java/com/banap/banap/ui/theme/Color.kt
@@ -0,0 +1,5 @@
+package com.banap.banap.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BRANCO = Color(0xFFFFFFFF)
---
+++ app/src/main/java/com/banap/banap/ui/theme/Type.kt
@@ -0,0 +1,6 @@
+package com.banap.banap.ui.theme
+
+import androidx.compose.material3.Typography
+
+val Typography = Typography(
+)
