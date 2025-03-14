e158bccbf995f37e73242d088eb10bd27e64523a
gilSFranco <gilmarsoaresfranco@gmail.com>
2025-03-14 17:47:41 -0300

    feat: new image added to ReadyToStart page

--- app/src/main/java/com/banap/banap/view/ReadyToStart.kt
+++ app/src/main/java/com/banap/banap/view/ReadyToStart.kt
@@ -3,9 +3,11 @@ package com.banap.banap.view
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -20,7 +22,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -52,65 +56,79 @@ fun ReadyToStart(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceBetween
         ) {
-            Column (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 40.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.tutorial_05),
-                    contentDescription = "Imagem de uma mulher com uma planta na mão",
+
+            Box {
+                Column(
                     modifier = Modifier
-                        .scale(1.5F)
-                )
-
-                Spacer(modifier = Modifier.height(60.dp))
-
-                Text(
-                    text = "Pronto para começar?",
-                    style = Typography.titleLarge,
-                    fontSize = 22.sp,
-                    color = VERDE_ESCURO
-                )
-
-                Spacer(modifier = Modifier.height(20.dp))
-
-                Text(
-                    text = "Este pequeno ‘tutorial’ é apenas uma curta demonstração do que nosso sistema tem a oferecer. Ainda há muito mais por vir!",
-                    textAlign = TextAlign.Center,
-                    style = Typography.bodyLarge,
-                    fontWeight = FontWeight.Normal,
-                    color = VERDE_ESCURO
-                )
-
-                Spacer(modifier = Modifier.height(25.dp))
-
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(
-                        space = 30.dp,
-                        alignment = Alignment.CenterHorizontally
-                    ),
-                    verticalAlignment = Alignment.CenterVertically
+                        .fillMaxWidth()
+                        .padding(horizontal = 40.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    repeat(5) { iteration ->
-                        val color = if (iteration == 4) VERDE_ESCURO else CINZA_CLARO
+                    Image(
+                        painter = painterResource(id = R.drawable.tutorial_05),
+                        contentDescription = "Imagem de uma mulher com uma planta na mão",
+                        modifier = Modifier
+                            .scale(1.5F)
+                    )
 
-                        Card(
-                            modifier = Modifier
-                                .size(10.dp),
-                            shape = CircleShape,
-                            colors = CardDefaults.cardColors(
-                                containerColor = color
-                            )
-                        ) {
+                    Spacer(modifier = Modifier.height(60.dp))
 
+                    Text(
+                        text = "Pronto para começar?",
+                        style = Typography.titleLarge,
+                        fontSize = 22.sp,
+                        color = VERDE_ESCURO
+                    )
+
+                    Spacer(modifier = Modifier.height(20.dp))
+
+                    Text(
+                        text = "Este pequeno ‘tutorial’ é apenas uma curta demonstração do que nosso sistema tem a oferecer. Ainda há muito mais por vir!",
+                        textAlign = TextAlign.Center,
+                        style = Typography.bodyLarge,
+                        fontWeight = FontWeight.Normal,
+                        color = VERDE_ESCURO
+                    )
+
+                    Spacer(modifier = Modifier.height(25.dp))
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(
+                            space = 30.dp,
+                            alignment = Alignment.CenterHorizontally
+                        ),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        repeat(5) { iteration ->
+                            val color = if (iteration == 4) VERDE_ESCURO else CINZA_CLARO
+
+                            Card(
+                                modifier = Modifier
+                                    .size(10.dp),
+                                shape = CircleShape,
+                                colors = CardDefaults.cardColors(
+                                    containerColor = color
+                                )
+                            ) {
+
+                            }
                         }
                     }
                 }
+
+                Image(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.linhas_ready_to_start),
+                    contentDescription = "Linhas da tela",
+                    modifier = Modifier
+                        .absoluteOffset(
+                            y = 220.dp
+                        )
+                        .scale(1.1F)
+                )
             }
+
 
             Column (
                 modifier = Modifier
@@ -118,7 +136,7 @@ fun ReadyToStart(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.spacedBy(20.dp)
             ) {
-                Button(
+                Button (
                     texto = "Cadastrar se",
                     modifier = Modifier
                         .padding(vertical = 11.dp, horizontal = 74.dp),
@@ -132,7 +150,7 @@ fun ReadyToStart(
                     defaultElevetion = 1.dp
                 )
 
-                Button(
+                Button (
                     texto = "Logar",
                     modifier = Modifier
                         .padding(vertical = 11.dp, horizontal = 100.dp),
@@ -146,6 +164,7 @@ fun ReadyToStart(
                     defaultElevetion = 1.dp
                 )
             }
+
         }
     }
 }
