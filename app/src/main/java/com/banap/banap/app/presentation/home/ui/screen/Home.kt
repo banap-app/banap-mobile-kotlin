7e735b4386ec55a1355770d0c28bdf01b5a294b5
gilSFranco <gilmarsoaresfranco@gmail.com>
2025-04-27 14:49:55 -0300

    refactor: splash screen and new loading screen

--- app/src/main/java/com/banap/banap/app/presentation/home/ui/screen/Home.kt
+++ app/src/main/java/com/banap/banap/app/presentation/home/ui/screen/Home.kt
@@ -2,16 +2,13 @@ package com.banap.banap.app.presentation.home.ui.screen
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -19,7 +16,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
@@ -33,9 +29,8 @@ import com.banap.banap.app.presentation.home.ui.components.Property
 import com.banap.banap.app.presentation.home.ui.components.RecentActivities
 import com.banap.banap.app.presentation.home.ui.components.Tasks
 import com.banap.banap.app.presentation.session.viewmodel.TokenViewModel
-import com.banap.banap.core.ui.components.SplashScreen
+import com.banap.banap.core.ui.components.LoadingScreen
 import com.banap.banap.core.ui.theme.BRANCO
-import com.banap.banap.core.ui.theme.VERDE_CLARO
 import com.banap.banap.domain.viewmodel.TokenVerificationViewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -79,9 +74,7 @@ fun Home(
         containerColor = BRANCO
     ) {
         if (isLoading) {
-            SplashScreen(
-                isFirstScreen = false
-            )
+            LoadingScreen()
         }
 
         if (isTokenValid) {
--- app/src/main/java/com/banap/banap/core/ui/components/SplashScreen.kt
+++ app/src/main/java/com/banap/banap/core/ui/components/SplashScreen.kt
@@ -1,6 +1,10 @@
 package com.banap.banap.core.ui.components
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,22 +17,49 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.banap.banap.R
+import com.banap.banap.core.ui.theme.BRANCO
 import com.banap.banap.core.ui.theme.CINZA_CLARO
 import com.banap.banap.core.ui.theme.Typography
 import com.banap.banap.core.ui.theme.VERDE_CLARO
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(
-    isFirstScreen: Boolean = true
+    navigationController: NavController
 ) {
+    val scale = remember { Animatable(0.8f) }
+    val alpha = remember { Animatable(0f) }
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 600,
+                easing = FastOutSlowInEasing
+            )
+        )
+        alpha.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 600
+            )
+        )
+        delay(3_000L)
+        navigationController.navigate("Tutorial")
+    }
+
     val navBarPaddingBottom = WindowInsets
         .navigationBars
         .asPaddingValues()
@@ -36,17 +67,17 @@ fun SplashScreen(
 
     Box(
         modifier = Modifier
-            .fillMaxSize()
+            .background(BRANCO)
+            .fillMaxSize(),
     ) {
-        if (isFirstScreen) {
-            Image(
-                imageVector = ImageVector.vectorResource(id = R.drawable.desenho_de_cima),
-                contentDescription = "Vetor de linhas",
-                modifier = Modifier
-                    .align(Alignment.TopEnd)
-                    .scale(1.2F)
-            )
-        }
+        Image(
+            imageVector = ImageVector.vectorResource(id = R.drawable.desenho_de_cima),
+            contentDescription = "Vetor de linhas",
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .scale(scale.value)
+                .alpha(alpha.value)
+        )
 
         Column (
             modifier = Modifier
@@ -60,38 +91,46 @@ fun SplashScreen(
                 painter = painterResource(id = R.drawable.banap),
                 contentDescription = "Logo do Banap",
                 modifier = Modifier
-                    .scale(0.9F)
+                    .scale(scale.value)
+                    .alpha(alpha.value)
             )
 
             CircularProgressIndicator(
                 strokeWidth = 4.dp,
-                modifier = Modifier.size(48.dp),
+                modifier = Modifier
+                    .scale(scale.value)
+                    .alpha(alpha.value)
+                    .size(48.dp),
                 color = VERDE_CLARO
             )
         }
 
-        if (isFirstScreen) {
-            Column (
+        Column (
+            modifier = Modifier
+                .padding(bottom = navBarPaddingBottom + 10.dp)
+                .align(Alignment.BottomCenter),
+            verticalArrangement = Arrangement.spacedBy(
+                space = 5.dp
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "from",
+                style = Typography.bodySmall,
+                color = CINZA_CLARO,
                 modifier = Modifier
-                    .padding(bottom = navBarPaddingBottom + 10.dp)
-                    .align(Alignment.BottomCenter),
-                verticalArrangement = Arrangement.spacedBy(
-                    space = 5.dp
-                ),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = "from",
-                    style = Typography.bodySmall,
-                    color = CINZA_CLARO
-                )
+                    .scale(scale.value)
+                    .alpha(alpha.value)
+            )
 
-                Text(
-                    text = "GreenHouse",
-                    style = Typography.bodySmall,
-                    color = VERDE_CLARO
-                )
-            }
+            Text(
+                text = "GreenHouse",
+                style = Typography.bodySmall,
+                color = VERDE_CLARO,
+                modifier = Modifier
+                    .scale(scale.value)
+                    .alpha(alpha.value)
+            )
         }
     }
 }
